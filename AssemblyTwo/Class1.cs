6cfec061e0a1121afc55e08baea9b22205ac18de
Chris <chriswfoster@gmail.com>
2018-06-23 23:47:37 -0500

    Internal and protected Internal

--- AssemblyTwo/Class1.cs
+++ AssemblyTwo/Class1.cs
@@ -4,21 +4,25 @@ using AssemblyOne; // to bring this in, I had to right click the AssemblyTwo ove
 
 namespace AssemblyTwo
 {
-    public class AssemblyTwoClassI
+  
+
+    public class AssemblyTwoClassI : AssemblyOneClassI
     {
         public void Print()
         {
             AssemblyOneClassI A1 = new AssemblyOneClassI();
-            // A1.ID = 88; // as  you can see, since I set it as internal, it will only allow it to be used locally to assembly.
+            base.IDS = 44; // you can't do A2.IDS, you can utilize base here to access what it's getting it's inheritance from;
+            AssemblyTwoClassI A2 = new AssemblyTwoClassI();
+            A2.IDS = 83;
         }
     }
 
-    public class AssemblyTwoClassII : AssemblyOneClassI
+    public class AssemblyTwoClassII
     {
         public void Print()
         {
-            AssemblyTwoClassI A2 = new AssemblyTwoClassI();
-            base.IDS = 44; // you can't do A2.IDS, you can utilize base here to access what it's getting it's inheritance from;
+            AssemblyOneClassI A1 = new AssemblyOneClassI(); // I guess I'm pulling the class AssemblyOneClass into here with this, and creating A1 with AssemblyOneClass's inheritance.
+            // A1.ID = 88; // as  you can see, since I set it as internal, it will only allow it to be used locally to assembly.
         }
     }
 }
