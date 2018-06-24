2417e9cdb8ef6bb561073e120ecbdae1debb567d
Chris <chriswfoster@gmail.com>
2018-06-23 23:11:09 -0500

    I think Im getting this...

--- AccessibilityTutorial/Program.cs
+++ AccessibilityTutorial/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace AccessibilityTutorial
+public class Customer
 {
-    class Program
+    private int _id; //private means it's only accessible through the containing type, which would be the class it's in... "Customer"
+    public int ID 
     {
-        static void Main(string[] args)
+        get {
+            return _id;
+        }
+        set
         {
-            Console.WriteLine("Hello World!");
+            _id = value;
         }
     }
+
+}
+
+public class MainClass
+{
+    private static void Main()
+    {
+        Customer C1 = new Customer(); // I create a new customer class named C1.
+       // Console.WriteLine(C1._id); // see the red error here, saying that the ID isn't available due to it being protected.
+        Console.WriteLine(C1.ID);
+    }
+
 }
