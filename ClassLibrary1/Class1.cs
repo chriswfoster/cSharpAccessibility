1e1043d5d2236513ceba914ad70512fb71ff5150
Chris <chriswfoster@gmail.com>
2018-06-23 23:47:45 -0500

    Internal and protected Internal

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 
-namespace ClassLibrary1
+namespace AssemblyOne
 {
-    public class Class1
+    public class AssemblyOneClassI
     {
+        internal int ID = 55; // INTERNAL is available ONLY to the classes in this assembly (AssemblyOne seen in the menu there -> 
+        protected internal int IDS = 83; // PROTECTED INTERNAL passes it on to other assemblies or classes that derive from this class (AssemblyOneClassI);
+    }
+    public class AssemblyOneClassII
+    {
+        public void SampleMethod()
+        {
+            AssemblyOneClassI A1 = new AssemblyOneClassI();
+            Console.WriteLine(A1.ID);
+
+        }
     }
 }
